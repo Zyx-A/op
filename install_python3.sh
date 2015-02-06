3430d7b2c358f4f2ab300e8f236461a16c70b4c0
root <root@vps_JP.(none)>
2015-02-06 23:48:19 +0800

    install

---
+++ install_python3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+version="3.4.2"
+installdir="/opt/python34"
+durl=https://www.python.org/ftp/python/$version/Python-$version.tgz
+
+
+
+yum install -y -q readline-devel gcc make
+
+cd /tmp
+wget -q $durl
+tar zxvf Python-$version.tgz
+cd Python-$version
+./configure --prefix=$installdir && make -j 2 && make install && ln -s $installdir/bin/python3 /usr/bin/python3
