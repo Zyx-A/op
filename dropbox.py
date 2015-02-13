98134019187dbd549cd1822ab0e6a84fcdcf065d
root <root@vps_JP.(none)>
2015-02-13 21:53:20 +0800

    support greater than 150MB

--- dropbox.py
+++ dropbox.py
@@ -5,7 +5,8 @@ import json
 import urllib.request
 import argparse
 
-
+url_chunk = 'https://api-content.dropbox.com/1/chunked_upload?param=val'
+url_chunk_commit = 'https://api-content.dropbox.com/1/commit_chunked_upload/auto/'
 url_metadata = 'https://api.dropbox.com/1/metadata/auto/'
 url_file_get = 'https://api-content.dropbox.com/1/files/auto/'
 url_file_put = 'https://api-content.dropbox.com/1/files_put/auto/'
@@ -26,6 +27,50 @@ def color(str,clr=''):
 	else:
 		return '\033[36;1m' + str + '\033[0m'
 
+def up_chunk(localfile,remote_path):
+	url0 = '%s&access_token=%s'%(url_chunk,access_token)
+	url_commit = '%s%s/%s?access_token=%s'%(url_chunk_commit,remote_path,os.path.basename(localfile),access_token)
+	DATA = open(localfile,'rb')
+	url2 = None
+	while 1:
+		cDATA = DATA.read(104857600)  # 1024 * 1024 * 100 = 100MB
+		if not cDATA: break
+		if url2:
+			url = url2
+		else:
+			url = url0
+		req = urllib.request.Request(url,data=cDATA,method='PUT')
+		req.add_header('Content-Type','application/x-www-form-urlencoded;')
+		try:
+			res = urllib.request.urlopen(req)
+		except urllib.error.HTTPError as e:
+			print(e.code)
+			print(e.fp.read().decode('utf-8'))
+			exit(1)
+		else:
+			pp = (json.loads(res.read().decode('utf-8')))
+			upid = pp['upload_id']
+			offset = pp['offset']
+			url2 = url0 + '&upload_id=%s&offset=%d'%(upid,offset)
+			print(json.dumps(pp,indent=4))
+	DATA.close()
+
+	#Commit
+	pdata = 'upload_id=%s'%upid
+	req = urllib.request.Request(url_commit,data=pdata.encode('utf-8'))
+	req.add_header('Content-Type','application/x-www-form-urlencoded;')
+	try:
+		res = urllib.request.urlopen(req)
+	except urllib.error.HTTPError as e:
+		print(e.code)
+		print(e.fp.read().decode('utf-8'))
+	else:
+		pp = (json.loads(res.read().decode('utf-8')))
+		print(json.dumps(pp,indent=4))
+
+
+
+
 def up_file(localfile,remote_path):
 	url = '%s%s/%s?access_token=%s&param=val'%(url_file_put,remote_path,os.path.basename(localfile),access_token)
 	#print(url)
@@ -41,6 +86,7 @@ def up_file(localfile,remote_path):
 	else:
 		pp = (json.loads(res.read().decode('utf-8')))
 		print(json.dumps(pp,indent=4))
+	DATA.close()
 
 
 def down_file(remote_path):
@@ -103,7 +149,8 @@ def main():
 		down_file(argvs.download)
 		return
 	if argvs.upload:
-		up_file(argvs.upload[0],argvs.upload[1])
+		up_chunk(argvs.upload[0],argvs.upload[1])
+		#up_file(argvs.upload[0],argvs.upload[1])
 		return
 	if argvs.list:
 		get_meta(argvs.list)
