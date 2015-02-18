1bc8f525ac11743012a038a13c1832f83444e810
root <root@vps_JP.(none)>
2015-02-18 14:36:19 +0800

    add wget ssl

--- install_python3.sh
+++ install_python3.sh
@@ -4,12 +4,12 @@ version="3.4.2"
 installdir="/opt/python34"
 durl=https://www.python.org/ftp/python/$version/Python-$version.tgz
 
-
-
-yum install -y -q readline-devel gcc make wget
+yum install -y -q readline-devel gcc make wget openssl-devel
 
 cd /tmp
 wget -q $durl
 tar zxvf Python-$version.tgz
 cd Python-$version
 ./configure --prefix=$installdir && make -j 2 && make install && ln -s $installdir/bin/python3 /usr/bin/python3
+
+
