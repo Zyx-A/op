7b74372a686eb3bebfcafe3c6d283242d28d8225
WillGhost <sdbc1178@gmail.com>
2015-11-21 20:39:07 +0800

    ?

--- init_centos.sh
+++ init_centos.sh
@@ -3,7 +3,10 @@
 yum update -y
 yum install -y tree vim lrzsz gcc make bind-utils zip unzip wget curl openssl-devel git python-devel
 
-\cp vimrc /etc/vimrc
+cd /tmp
+rm -f vimrc
+wget https://raw.githubusercontent.com/WillGhost/op/master/vimrc
+mv -f vimrc /etc/vimrc
 
 pip install -U pip
 
