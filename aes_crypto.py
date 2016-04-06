d251d52f7ad4bab8ae2d33902117c24da7cf7484
WillGhost <sdbc1178@gmail.com>
2016-04-06 18:20:33 +0800

    NEW AES

--- aes_crypto.py
+++ aes_crypto.py
@@ -17,3 +17,24 @@ def decrypt(passphrase):
 	DecodeAES = lambda c,e: c.decrypt(base64.b64decode(e)).rstrip(padding)
 	return DecodeAES(cipher,passphrase)
 
+
+
+####
+# NEW
+
+key = 'abcdefhigklmnopq'
+
+def aes_encrypt(message, key):
+    block_size = 16
+    pad = lambda s: s + (block_size - len(s) % block_size) * chr(block_size - len(s) % block_size)
+    cipher = AES.new(key)
+    EncodeAES = lambda c,s: base64.b64encode(c.encrypt(pad(s)))
+    return EncodeAES(cipher,message)
+
+def aes_decrypt(passphrase, key):
+    passphrase = base64.b64decode(passphrase)
+    unpad = lambda s : s[:-ord(s[len(s)-1:])]
+    iv = passphrase[:16]
+    cipher = AES.new(key)
+    return unpad(cipher.decrypt(passphrase))
+
